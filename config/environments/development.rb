11ff9940a82380d7524cb2d103a01e8d77204abb
rongmic <rongmic@gmail.com>
2012-12-17 17:00:28 +0800

    add mailer and login

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -21,8 +21,20 @@ Depot::Application.configure do
   config.consider_all_requests_local       = true
   config.action_controller.perform_caching = false
 
+  config.action_mailer.delivery_method = :smtp
+
+  config.action_mailer.smtp_settings = {
+    :address        => "smtp.gmail.com",
+    :port           => 587,
+    :domain         => "rongmic@gmail.com",
+    :authentication => "plain",
+    :user_name      => "rongmic",
+    :password       => "rongming&1203",
+    :enable_stareels_auto => true
+  }
+
   # Don't care if the mailer can't send
-  config.action_mailer.raise_delivery_errors = false
+  config.action_mailer.raise_delivery_errors = true
 
   # Print deprecation notices to the Rails logger
   config.active_support.deprecation = :log
